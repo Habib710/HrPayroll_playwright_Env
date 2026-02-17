792c0cca27300d6683c3f90d4db5d0335122a387
Habib710 <mdhabib71024@gmail.com>
2026-02-17 13:56:24 +0600

    fix and update the login page action

--- tests/PageSpec/login.spec.ts
+++ tests/PageSpec/login.spec.ts
@@ -1,36 +1,26 @@
 import { expect, test } from "@playwright/test";
 import { Login } from "../pageActions/loginAction";
 
+test.describe('Login Page Testing', () => {
 
-
-
-
-test.describe('Login Page testing', () => {
     let login: Login;
 
-
-    test("Successful Sign Up with Valid credidantal user", async ({ page }) => {
+    test.beforeEach(async ({ page }) => {
         login = new Login(page);
         await page.goto("https://hrpayroll.hrsoftbd.net");
         await login.clickOnLoginIcon();
-        await login.login("sabah@gmail.com", "123456");
+    });
 
+    test("Successful login with valid credentials", async ({ page }) => {
+        await login.login("sabah@gmail.com", "123456");
         await expect(page).toHaveURL(/dashboard/);
-
-
     });
 
-    test("unsuccessful Sign Up with InValid credidantal user", async ({ page }) => {
-        login = new Login(page);
-        await page.goto("https://hrpayroll.hrsoftbd.net");
-        await login.clickOnLoginIcon();
-        await login.login("sabah@gmail.com", "123456");
+    test("Unsuccessful login with invalid email", async ({ page }) => {
+        await login.login("invalid@email.com", "123456");
 
-        const message = page.getByRole('alert')
-
+        const message = page.getByRole('alert');
         await expect(message).toBeVisible();
-
-
     });
 
-})
+});
