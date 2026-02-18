5c05f6b236a50fd546bae2018dc94a38d78afada
Habib710 <mdhabib71024@gmail.com>
2026-02-18 11:10:45 +0600

    profile page action added

---
+++ tests/pageActions/profilepageAction.ts
@@ -0,0 +1,22 @@
+import { Page, Locator } from '@playwright/test';
+import { profileObject } from '../PageObjects/profileObject';
+
+export class profile {
+
+    readonly page: Page;
+    readonly profileObject: profileObject;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.profileObject = new profileObject(page);
+    }
+    async clickOnProfileIcon() {
+        await this.profileObject.profileIcon.click();
+    }
+
+    async clickOnProfileLink() {
+        await this.profileObject.profileLink.click();
+    }
+
+
+}
